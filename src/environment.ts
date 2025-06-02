82cfbe8475eb1bde06571dcdb602dd1d1f04c152
Eduardo Viana Pessoa <eduardo.vianapessoa@vivint.com>
2025-06-02 17:33:12 -0600

    Auth Implementation

---
+++ src/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: 'AIzaSyBvnxa8j6hvrpo9BDWmBmnpNRJlD2LxUyo',
+        authDomain: 'xpehotels-dfe-2025.firebaseapp.com',
+        projectId: 'xpehotels-dfe-2025',
+        storageBucket: 'xpehotels-dfe-2025.firebasestorage.app',
+        messagingSenderId: '838594472217',
+        appId: '1:838594472217:web:3a0d449fc7253b98d2fd88',
+    }
+};
